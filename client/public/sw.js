a69ec3af9dc65934321e525a8571e57456eaf31c
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-08-19 01:34:57 +0530

    Update sw.js service worker

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'employees-attendance-v1';
+const CACHE_NAME = 'employees-attendance-v2';
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/logo.png'];
 
 self.addEventListener('install', (event) => {
